2be3842a9c50e7c8bdb0045c802a1ee29c311290
tiago <tiago@DESKTOP-00HI8IK>
2023-11-02 19:12:29 -0500

    Agregar archivos de proyecto.

---
+++ PRACTICA 2 UNIDAD 3/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace PRACTICA_2_UNIDAD_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Arial", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(224, 34);
+            label1.Name = "label1";
+            label1.Size = new Size(308, 96);
+            label1.TabIndex = 0;
+            label1.Text = "Dado como dato el sueldo\r\nde un trabajador, el programa\r\naplica un aumento del 15%\r\nsi el sueldo es inferior a 400000";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Arial", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Location = new Point(145, 172);
+            label2.Name = "label2";
+            label2.Size = new Size(200, 24);
+            label2.TabIndex = 1;
+            label2.Text = "INGRESAR SUELDO";
+            label2.Click += label2_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Arial", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.Location = new Point(177, 357);
+            label3.Name = "label3";
+            label3.Size = new Size(168, 24);
+            label3.TabIndex = 2;
+            label3.Text = "NUEVO SUELDO";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(380, 172);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(125, 27);
+            textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(380, 357);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(125, 27);
+            textBox2.TabIndex = 4;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Arial", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.Location = new Point(169, 259);
+            button1.Name = "button1";
+            button1.Size = new Size(133, 29);
+            button1.TabIndex = 5;
+            button1.Text = "DECISIÓN";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Arial", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            button2.Location = new Point(485, 259);
+            button2.Name = "button2";
+            button2.Size = new Size(135, 29);
+            button2.TabIndex = 6;
+            button2.Text = "BORRAR";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Button button1;
+        private Button button2;
+    }
+}
